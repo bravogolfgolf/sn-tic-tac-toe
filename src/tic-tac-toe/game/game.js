973f9ca01cae42e44ff4b95ba4b6e751a08d6636
Brian Gibson <gibsonbg@me.com>
2022-05-05 09:41:46 -0400

    Adds list of moves

--- src/tic-tac-toe/game/game.js
+++ src/tic-tac-toe/game/game.js
@@ -29,6 +29,18 @@ createCustomElement('tic-tac-toe-game', {
 		const history = state.history;
 		const current = history[history.length - 1];
 		const winner = calculateWinner(current.squares);
+
+		const moves = history.map((step, move) => {
+			const desc = move ?
+				'Go to move #' + move :
+				'Go to game start';
+			return (
+				<li key={move}>
+					<button onClick={() => this.jumpTo(move)}>{desc}</button>
+				</li>
+			);
+		});
+
 		let status;
 		if (winner) {
 			status = 'Winner: ' + winner;
@@ -42,7 +54,7 @@ createCustomElement('tic-tac-toe-game', {
 				</div>
 				<div className="game-info">
 					<div>{status}</div>
-					<ol>{/* TODO */}</ol>
+					<ol>{moves}</ol>
 				</div>
 			</div>
 		);
