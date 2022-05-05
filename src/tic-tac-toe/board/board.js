ca21c95b470f462033ad3e516d774457d8abe1d2
Brian Gibson <gibsonbg@me.com>
2022-05-05 09:33:09 -0400

    Moves control up to game; Adds history

--- src/tic-tac-toe/board/board.js
+++ src/tic-tac-toe/board/board.js
@@ -4,37 +4,16 @@ import '../square'
 import styles from '../style.css';
 
 createCustomElement('game-board', {
-	initialState: {
-		squares: Array(9).fill(null),
-		xIsNext: true,
-	},
-	actionHandlers: {
-		SQUARE_CLICKED: {
-			effect: ({action, state, updateState}) => {
-				const squares = state.squares.slice();
-				if (calculateWinner(squares) || squares[action.payload.index]) {
-					return;
-				}
-				squares[action.payload.index] = state.xIsNext ? 'X' : 'O';
-				updateState({squares: squares, xIsNext: !state.xIsNext});
-			}
-		}
+	properties: {
+		squares: {},
 	},
 	renderer: {type: snabbdom},
-	view: (state) => {
+	view: ({properties: {squares}}) => {
 		function renderSquare(i) {
-			return <board-square className="square" index={i} value={state.squares[i]}/>;
-		}
-		const winner = calculateWinner(state.squares);
-		let status;
-		if (winner) {
-			status = 'Winner: ' + winner;
-		} else {
-			status = 'Next player: ' + (state.xIsNext ? 'X' : 'O');
+			return <board-square className="square" index={i} value={squares[i]}/>;
 		}
 		return (
 			<div>
-				<div className="status">{status}</div>
 				<div className="board-row">
 					{renderSquare(0)}
 					{renderSquare(1)}
@@ -55,23 +34,3 @@ createCustomElement('game-board', {
 	},
 	styles
 });
-
-function calculateWinner(squares) {
-	const lines = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[0, 4, 8],
-		[2, 4, 6],
-	];
-	for (let i = 0; i < lines.length; i++) {
-		const [a, b, c] = lines[i];
-		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-			return squares[a];
-		}
-	}
-	return null;
-}
--- src/tic-tac-toe/game/game.js
+++ src/tic-tac-toe/game/game.js
@@ -1,10 +1,72 @@
 import {createCustomElement} from '@servicenow/ui-core';
 import snabbdom from '@servicenow/ui-renderer-snabbdom';
-import view from './view'
+import '../board';
 import styles from '../style.css';
 
 createCustomElement('tic-tac-toe-game', {
+	initialState: {
+		history: [{
+			squares: Array(9).fill(null)
+		}],
+		xIsNext: true,
+	},
+	actionHandlers: {
+		SQUARE_CLICKED: {
+			effect: ({action, state, updateState}) => {
+				const history = state.history;
+				const current = history[history.length - 1];
+				const squares = current.squares.slice();
+				if (calculateWinner(squares) || squares[action.payload.index]) {
+					return;
+				}
+				squares[action.payload.index] = state.xIsNext ? 'X' : 'O' || null;
+				updateState({history: history.concat([{squares: squares}]), xIsNext: !state.xIsNext});
+			}
+		}
+	},
 	renderer: {type: snabbdom},
-	view,
+	view: (state) => {
+		const history = state.history;
+		const current = history[history.length - 1];
+		const winner = calculateWinner(current.squares);
+		let status;
+		if (winner) {
+			status = 'Winner: ' + winner;
+		} else {
+			status = 'Next player: ' + (state.xIsNext ? 'X' : 'O');
+		}
+		return (
+			<div className="game">
+				<div className="board">
+					<game-board squares={current.squares}/>
+				</div>
+				<div className="game-info">
+					<div>{status}</div>
+					<ol>{/* TODO */}</ol>
+				</div>
+			</div>
+		);
+	},
 	styles,
 });
+
+function calculateWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		}
+	}
+	return null;
+}
+
